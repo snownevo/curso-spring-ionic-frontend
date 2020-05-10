2afe5d69652dce27964dcb404480b8f86994dcae
snownevo <manuematheus27@hotmail.com>
2020-05-10 05:49:24 -0300

    Criando pagina de carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { StorageService } from '../services/storage.services';
 import { ClienteService } from "../services/domain/cliente.service";
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 import { ProdutoService } from '../services/domain/produto.service';
+import { CartService } from '../services/domain/cart.service';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,8 @@ import { ProdutoService } from '../services/domain/produto.service';
     AuthService,
     StorageService,
     ClienteService,
-    ProdutoService
+    ProdutoService,
+    CartService
 
   ]
 })
