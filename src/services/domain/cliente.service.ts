9bbb2c8b42cdf46f35a7830cf31c8b23f522d2d9
snownevo <manuematheus27@hotmail.com>
2020-05-11 03:40:52 -0300

    Buscando enderecos do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -8,8 +8,8 @@ import { StorageService } from "../storage.services";
 export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {
     }
-    findByEmail(email: string): Observable<ClienteDTO> {
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
     getImageFromBucket(id: string): Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
