8479850f4139da463668ef0f01abe6870f06e99d
snownevo <manuematheus27@hotmail.com>
2020-05-12 02:17:39 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 @IonicPage()
 @Component({
@@ -23,11 +24,12 @@ export class OrderConfirmationPage {
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
-  
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
+
     this.pedido=this.navParams.get('pedido');
-  
-  
+
+
   }
 
   ionViewDidLoad() {
@@ -52,5 +54,23 @@ private findEndereco(id:string, list: EnderecoDTO[]) : EnderecoDTO{
 total(): number {
   return this.cartService.total();
 }
+
+back() {
+  this.navCtrl.setRoot('CartPage');
+}
+
+checkout() {
+  this.pedidoService.insert(this.pedido)
+    .subscribe(response => {
+      this.cartService.createOrClearCart();
+      console.log(response.headers.get('location'));
+    },
+    error => {
+      if (error.status == 403) {
+        this.navCtrl.setRoot('HomePage');
+      }
+    });
+}
+
 
 }
